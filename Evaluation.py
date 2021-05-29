9616c63725a150e7ef6b69c138fce86999792c6e
reemnasserr <64875457+reemnasserr@users.noreply.github.com>
2021-05-29 14:07:20 +0200

    modifying image paths

--- Evaluation.py
+++ Evaluation.py
@@ -8,15 +8,15 @@ class Evaluation:
     def __init__(self):
         
         self.distance=0
-        self.summation_intersection=0
+        
         
     def NormalizedDifference(self, hist_queryimage , hist_database_image , database_image):
         
         height,width = database_image.shape[:2]
         num_of_pixels = width*height
-               
+        self.summation_intersection=0       
         for (q, d) in zip(hist_queryimage, hist_database_image):
-
+           
             self.summation_intersection += min(q, d)
   
         
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from Evaluation import *
 import  os
 
 
-image_path = r'C:/Users/reemn/Desktop/multi_images' #database images path
-query = r'C:/Users/reemn/Desktop/multi_images/orange1.jpg' #input query from UI
+image_path = r'./multi_images' #database images path
+query = r'./multi_images/orange1.jpg' #input query from UI
 
 
 
@@ -14,4 +14,4 @@ for imagefile in os.listdir(image_path): #loop on images for the 1st time to be 
     image =  cv2.imread(image_path+str('/')+imagefile)     
     a = FeatureExtractor(image)  
     c = a.ColorHistogram()   # save features in database
-    
+
