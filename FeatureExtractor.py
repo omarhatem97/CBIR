3c98023570cc4e6398104d317edb9ff9bc5b34ff
omarhatem97 <omarhatem221@gmail.com>
2021-05-30 18:45:10 +0200

    add meanclolor to feature extractor

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,10 +1,34 @@
 import cv2 as cv
 import numpy as np
+from matplotlib import colors, pyplot as plt
 
 class FeatureExtractor:
     
     def __init__(self,image):
         self.image=image
+    
+
+    def Histogram(self):
+        #Histogram algorithm
+
+        image = cv.cvtColor(self.image, cv.COLOR_BGR2HSV )
+        image = cv.resize(image,(512,512))
+        features = []
+        
+        (h, w) = image.shape[:2]
+        x,y= (int(w*0.5) , int(h*0.5)) #center
+        
+        for row in range(0 ,w , x):
+            for coloumn in range(0,h , y):
+                
+                window = image[row:row+x,coloumn:coloumn+y]  
+                
+                #hardcoded bins
+                hist = cv.calcHist([window], [0,1,2],None,[10,10,4],[0, 180, 0, 256,0,256])  
+                hist =  cv.normalize(hist,hist).flatten()
+                features.extend(hist)
+        return features
+
     
     def ColorLayout(self):
         #color layout algorithm
@@ -28,6 +52,22 @@ class FeatureExtractor:
                 hist =  cv.normalize(hist,hist).flatten()
                 features.extend(hist)
         return features, epsilon
+
+    
+    def mean_color(img):
+
+        image_bgr=cv.imread(img,cv.IMREAD_COLOR)
+        #cv.imshow('cv',image_bgr)
+        #cv.waitKey(0) 
+        #cv.destroyAllWindows() 
+        
+        colors=cv.mean(image_bgr)
+    
+        observation=np.array([(colors[2],colors[1],colors[0])])
+        print(observation)
+        plt.imshow(observation)
+        plt.show()
+        return observation
                 
                 
 
