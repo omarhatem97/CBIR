8448f7071aca45ddc569e444f7e21721aa3f7baf
panse <47269934+panse98@users.noreply.github.com>
2021-05-30 23:56:28 +0200

    Add files via upload

--- mean color.py
+++ mean color.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import colors, pyplot as plt
-
+threshold=155
 def mean_color(img):
     image_bgr=cv2.imread(img,cv2.IMREAD_COLOR)
     #cv2.imshow('cv',image_bgr)
@@ -11,7 +11,7 @@ def mean_color(img):
     colors=cv2.mean(image_bgr)
    
     observation=np.array([(colors[2],colors[1],colors[0])])
-    print(observation)
+    #print(observation)
     plt.imshow(observation)
     plt.show()
     return observation
@@ -22,7 +22,13 @@ def compare(img1,img2):
     x=mean_color(img1)
     y=mean_color(img2)
     diff=np.abs(x-y)
-    print(diff)
+    error=np.sum(diff)
+    if error<threshold:
+        
+        print(error)
+    else:
+        print('NOO')
+
 
     
 
@@ -32,5 +38,5 @@ if __name__ == "__main__":
  # x= mean_color('orange.jpg')
   
  # y=mean_color('orange2.jpg')
- compare('orange.jpg','orange2.jpg')
+ compare('apple.jpg','apple1.jpg')
  
