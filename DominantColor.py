4e11063a7a65845face571b18497e20db4b7a34d
panse <47269934+panse98@users.noreply.github.com>
2021-06-03 20:28:01 +0200

    Add files via upload

--- DominantColor.py
+++ DominantColor.py
@@ -4,16 +4,18 @@ from matplotlib import colors, pyplot as plt
 
 
 
-def unique_count_app(a):
+def unique_count_app(img):
+    a=cv2.imread(img)
+    a=cv2.resize(a,(512,512))
     colors, count = np.unique(a.reshape(-1,a.shape[-1]), axis=0, return_counts=True)
     #print(colors[count.argmax()])
 
     return colors[count.argmax()]
 
-
-img1=cv2.imread('orange.jpg')
+### for debugging###
+img1=cv2.imread('orange3.jpg')
 img2=cv2.imread('orange1.jpg')
-x=unique_count_app(img1)
-y=unique_count_app(img2)
+x=unique_count_app('orange3.jpg')
+#y=unique_count_app(img2)
 print(x)
-print(y)
+#print(y)
